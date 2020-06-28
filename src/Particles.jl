d42d82d74ac4d3b956ce3ae8771ddc0c152dab73
Federico Stra <stra.federico@gmail.com>
2020-06-28 19:47:25 +0200

    Add function `make_velocity`

--- src/Particles.jl
+++ src/Particles.jl
@@ -9,4 +9,38 @@ include("plotting.jl")
 export empty_like, pwc_density
 export plot_density, plot_density!, plot_linear_density!
 
+export make_velocity
+
+"""
+    make_velocity(V::Function, Wprime::Function, mobility::Function)
+
+Creates a function `velocity(dx, x, p, t)` that computes the velocity of the particles
+under the influence of an external force `V`, a mutual interaction `Wprime` and the
+congestion given by `mobility`.
+"""
+function make_velocity(V::Function, Wprime::Function, mobility::Function)
+    function velocity(dx, x, p, t)
+        R = pwc_density(x)
+        len = length(x)
+        for i in 1:len
+            v::eltype(x) = 0
+            for j in 1:i-1
+                v += Wprime(x[j] - x[i])
+            end
+            for j in i+1:len
+                v += Wprime(x[j] - x[i])
+            end
+            v /= len
+            v += V(x[i])
+            if v < 0
+                mob = mobility(R[i])
+            else
+                mob = mobility(R[i+1])
+            end
+            dx[i] = v * mob
+        end
+    end
+    velocity
+end
+
 end # module
