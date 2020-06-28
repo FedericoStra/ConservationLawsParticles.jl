0f4cf9044704d716b7110318ec62b7a6858c8cb2
Federico Stra <stra.federico@gmail.com>
2020-06-28 19:42:05 +0200

    Convert tabs to 4 spaces

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,12 +2,12 @@ using Particles
 using Test
 
 @testset "empty_like" begin
-	@test length(empty_like([1,2,3])) == 3
-	@test typeof(empty_like([1,2,3])) == Vector{Int}
+    @test length(empty_like([1,2,3])) == 3
+    @test typeof(empty_like([1,2,3])) == Vector{Int}
 end
 
 @testset "pwc_density" begin
-	@test pwc_density([0, 1, 3]) == [0.0, 0.5, 0.25, 0.0]
-	@test pwc_density([0., 1.]) == [0.0, 1.0, 0.0]
-	@test pwc_density([0.]) == [0.0, 0.0]
+    @test pwc_density([0, 1, 3]) == [0.0, 0.5, 0.25, 0.0]
+    @test pwc_density([0., 1.]) == [0.0, 1.0, 0.0]
+    @test pwc_density([0.]) == [0.0, 0.0]
 end
