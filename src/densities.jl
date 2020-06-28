41078bacaf55abd9a85e53e0920ae2222442d72d
Federico Stra <stra.federico@gmail.com>
2020-06-28 19:47:08 +0200

    Reformat docs

--- src/densities.jl
+++ src/densities.jl
@@ -5,8 +5,11 @@ $(TYPEDSIGNATURES)
 
 Returns the piecewise constant probability density from the quantile particle positions.
 
-Let the quantile particles be at positions `x₀, x₁, …, xₙ` (remember that in Julia they are actually `x[1], x[2], ..., x[n], x[n+1]`).
-The returned array `R` is such that `R[1] = R[n+2] = 0` and `R[i] = 1 / (N * (x[i] - x[i-1]))` for the intermediate indices (this formula holds also for the first and last entry if we assume `x[0] = -∞` and `x[n+2] = ∞`).
+Let the quantile particles be at positions `x₀, x₁, …, xₙ` (remember that in Julia
+they are actually `x[1], x[2], ..., x[n], x[n+1]`).
+The returned array `R` is such that `R[1] = R[n+2] = 0` and `R[i] = 1 / (N * (x[i] - x[i-1]))`
+for the intermediate indices (this formula holds also for the first and last entry
+if we assume `x[0] = -∞` and `x[n+2] = ∞`).
 
 # Examples
 
