345cb370297d2464927f3619644fbe47bad8ab30
Federico Stra <stra.federico@gmail.com>
2020-07-07 13:01:29 +0200

    Optimize `make_velocities` (convolution kernel)
    Optimize `make_velocities` by calling an external convolution kernel
    (`total_interaction`) instead of repeatedly calling directly the
    untyped function `Wprimes[spec][other]`.

--- src/velocities.jl
+++ src/velocities.jl
@@ -30,7 +30,8 @@ function make_velocity(V::Function, Wprime::Function, mobility::Function)
     velocity
 end
 
-function make_velocities(
+export make_velocities_
+function make_velocities_(
         Vs::Tuple{Vararg{Any,N}},
         Wprimes::Tuple{Vararg{Tuple{Vararg{Any,N}},N}},
         mobilities::Tuple{Vararg{Any,N}}
@@ -74,6 +75,51 @@ function make_velocities(
                 end
                 # external velocity
                 v += Vs[spec](x[i])
+                if v < 0
+                    mob = mobilities[spec](dens[spec][:, 1, i]...)
+                else
+                    mob = mobilities[spec](dens[spec][:, 2, i]...)
+                end
+                dx.x[spec][i] = v * mob
+            end
+        end
+    end
+    velocities
+end
+
+export total_interaction, total_interaction_
+
+function total_interaction(Wprime, ys::AbstractVector{<:Real}, x::Real)
+    sum(Wprime(y - x) for y in ys) / length(ys)
+end
+
+function total_interaction_(Wprime, ys::AbstractVector{<:Real}, x::Real)
+    T = promote_type(eltype(ys), typeof(x))
+    w::T = 0
+    for y in ys
+        w += Wprime(y - x)
+    end
+    w / length(ys)
+end
+
+function make_velocities(
+        Vs::Tuple{Vararg{Any,N}},
+        Wprimes::Tuple{Vararg{Tuple{Vararg{Any,N}},N}},
+        mobilities::Tuple{Vararg{Any,N}}
+        ) where N
+    function velocities(
+            dx::ArrayPartition{F, T},
+            x::ArrayPartition{F, T},
+            p,
+            t
+            ) where F where T<:Tuple{Vararg{AbstractVector{<:Real}}}
+        dens = pwc_densities(x.x...)
+        for spec in 1:N
+            for i in 1:length(x.x[spec])
+                v::F = Vs[spec](x[i])
+                for other in 1:N
+                    v += total_interaction(Wprimes[spec][other], x.x[other], x.x[spec][i])
+                end
                 if v < 0
                     mob = mobilities[spec](dens[spec][:, 1, i]...)
                 else
