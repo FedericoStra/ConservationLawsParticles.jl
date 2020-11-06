63321b2bb74bd8c550fea2d64368642b277cdb7a
Federico Stra <stra.federico@gmail.com>
2020-11-06 18:23:05 +0100

    Add convenience macro to generate methods for time independent functions

--- src/utils.jl
+++ src/utils.jl
@@ -22,3 +22,42 @@ function gaussian_particles(width::Real, number::Integer)
     e = erf(width)
     erfinv.(range(-e, e, length=number))
 end
+
+
+export @time_independent
+
+"""
+Automatically define a method which takes time as first argument and discards it.
+
+# Examples
+The definition
+```julia
+@time_independent V(x) = -x^3
+```
+is equivalent to
+```julia
+V(x) = -x^3
+V(t, x) = V(x)
+```
+
+This also works with more than one argument, for instance
+```julia
+@time_independent V(x₁, x₂) = x₁ * x₂
+```
+is equivalent to
+```julia
+V(x₁, x₂) = x₁ * x₂
+V(t, x₁, x₂) = V(x₁, x₂)
+```
+"""
+macro time_independent(code)
+    @assert code isa Expr
+    @assert code.head == :(=) || code.head == :function
+    lhs = code.args[1]
+    @assert lhs.head == :call
+    f = lhs.args[1]
+    quote
+        $(esc(code))
+        $(esc(f))(t, args...) = $(esc(f))(args...)
+    end
+end
