eb85f703ad67d858b4ca7b2e400e8821e170686b
Federico Stra <stra.federico@gmail.com>
2021-08-02 16:04:32 +0200

    Make example fields time independent

--- src/examples.jl
+++ src/examples.jl
@@ -1,11 +1,11 @@
 export V, V2, W_attr, W_rep, Wprime_attr, Wprime_rep, mobility, mobρ, mobσ
 
-V(r) = - r^3
-V2(r) = - (r-1)^3
-W_attr(r) = 5log(abs(r) + 1)
-W_rep(r) = -5log(abs(r) + 1)
-Wprime_attr(r) = 5 * sign(r) / (abs(r) + 1)
-Wprime_rep(r) = - 5 * sign(r) / (abs(r) + 1)
+@time_independent V(r) = - r^3
+@time_independent V2(r) = - (r-1)^3
+@time_independent W_attr(r) = 5log1p(abs(r))
+@time_independent W_rep(r) = -5log1p(abs(r))
+@time_independent Wprime_attr(r) = 5 * sign(r) / (abs(r) + 1)
+@time_independent Wprime_rep(r) = - 5 * sign(r) / (abs(r) + 1)
 mobility(ρ) = max(1 - ρ, 0)
 mobility(ρ, σ) = max(1 - ρ - σ, 0)
 mobρ(ρ, σ) = max(1 - ρ - 0.5σ, 0)
