164dba944c0a21da661554f373f6e0c6b6168db7
Federico Stra <stra.federico@gmail.com>
2021-08-03 17:31:07 +0200

    Add another example of interaction potential

--- src/examples.jl
+++ src/examples.jl
@@ -2,7 +2,7 @@ module Examples
 
 using ..ConservationLawsParticles: @time_independent
 
-export V, V2, W_attr, W_rep, Wprime_attr, Wprime_rep, mob, mobρ, mobσ
+export V, V2, W_attr, W_rep, Wprime_attr, Wprime_rep, W, Wprime, mob, mobρ, mobσ
 
 @time_independent V(r) = - r^3
 @time_independent V2(r) = - (r-1)^3
@@ -10,6 +10,8 @@ export V, V2, W_attr, W_rep, Wprime_attr, Wprime_rep, mob, mobρ, mobσ
 @time_independent W_rep(r) = -5log1p(abs(r))
 @time_independent Wprime_attr(r) = 5 * sign(r) / (abs(r) + 1)
 @time_independent Wprime_rep(r) = - 5 * sign(r) / (abs(r) + 1)
+@time_independent W(r) = - 1 / (abs(r) + 1)
+@time_independent Wprime(r) = sign(r) / (abs(r) + 1)^2
 mob(ρ) = max(1 - ρ, 0)
 mob(ρ, σ) = max(1 - ρ - σ, 0)
 mobρ(ρ, σ) = max(1 - ρ - 0.5σ, 0)
