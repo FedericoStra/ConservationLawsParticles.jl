29be14b8b576ae7a2880938505d5ca038646b870
Federico Stra <stra.federico@gmail.com>
2021-08-03 11:44:35 +0200

    Do not export `make_velocity`/`make_velocities`

--- src/ConservationLawsParticles.jl
+++ src/ConservationLawsParticles.jl
@@ -12,7 +12,6 @@ include("examples.jl")
 
 export empty_like
 export pwc_density, pwc_densities
-export make_velocity, make_velocities
 export plot_density, plot_density!, plot_linear_density!
 
 end # module
