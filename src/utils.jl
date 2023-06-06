a6e8175fa24f869f3c6053b65f26dc62d3330ecc
Federico Stra <stra.federico@gmail.com>
2023-06-06 10:44:41 +0200

    Cleanup utils: remove deprecated `empty_like` and document `gaussian_particles`

--- src/utils.jl
+++ src/utils.jl
@@ -1,30 +1,20 @@
-"""
-    empty_like(x::AbstractArray)
-
-Creates an empty array with the same `size` and `eltype` of `x`.
-
-# Examples
-```julia-repl
-julia> empty_like([1 2 3; 4 5 6])
-2×3 Array{Int64,2}:
- 0  140456850103040                1
- 1                3  140456850103041
-```
-"""
-function empty_like end
-@deprecate empty_like(x::AbstractArray) similar(x)
-# empty_like(x::AbstractArray) = Array{eltype(x)}(undef, size(x))
-
 using SpecialFunctions
-export gaussian_particles
+
+export gaussian_particles, @time_independent
+
+
+"""
+    gaussian_particles(width::Real, number::Integer)
+
+Generates `number` particles defining a Gaussian distribution with variance `1/2` truncated
+to the interval `[-width, width]`.
+"""
 function gaussian_particles(width::Real, number::Integer)
     width > 0 && number > 1 || error("requirements: width > 0 and number > 1")
     e = erf(width)
     erfinv.(range(-e, e, length=number))
 end
 
-
-export @time_independent
 
 """
 Automatically define a method which takes time as first argument and discards it.
